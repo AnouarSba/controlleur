4bfb2c756baea82b4b34785f541d0a5917f67bec
AnouarSba <blackvinom12445@gmail.com>
2024-07-03 15:13:02 +0100

    fix ctrl alert

--- app/Http/Controllers/ControlController.php
+++ app/Http/Controllers/ControlController.php
@@ -946,9 +946,9 @@ public function locate(Request $request)
       $S = User::where('id',  auth()->user()->id)->first()->salaire;
       $M = User::where('id',  auth()->user()->id)->first()->salaire_mois;
       
-    DB::statement("SET SQL_MODE=''");
-   $row = Position::create(['user_id' => $y, 'bus_id' => $bus,'lat' => $lat, 'lang' => $lang ]);
-    $row = Report::create(['user_id' => $y, 'bus_id' => $bus,'ligne_id' => $ligne, 'num' => $num, 'kabid_id' => $kabid,'chauffeur_id' => $chauff, 'place' => $place ]);
+//     DB::statement("SET SQL_MODE=''");
+//    $row = Position::create(['user_id' => $y, 'bus_id' => $bus,'lat' => $lat, 'lang' => $lang ]);
+//     $row = Report::create(['user_id' => $y, 'bus_id' => $bus,'ligne_id' => $ligne, 'num' => $num, 'kabid_id' => $kabid,'chauffeur_id' => $chauff, 'place' => $place ]);
   
     $buses = Bus::get();
     $avance = Avance::where('emp_id', auth()->user()->id)->where('month', date('m'))->where('year', date('Y'))->first();
